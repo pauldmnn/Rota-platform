4d8283eaf9be50074b77fe50fbe51e31d21c5065
Paul-vasile Dominan <pauldmnn@gmail.com>
2024-12-14 17:07:37 +0000

    Added request urls and request html

--- rota/urls.py
+++ rota/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"), 
-    path("manage_tota/", views.manage_rota, name="manage_rota")
+    path("manage_tota/", views.manage_rota, name="manage_rota"),
+    path("request_day/", views.request_day, name="request_day"),
 ]
