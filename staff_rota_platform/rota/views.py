c6007603890b05c1f940217b8a3fc263a73938be
Paul-vasile Dominan <pauldmnn@gmail.com>
2024-12-14 15:40:44 +0000

    Added viewes for rota management

--- staff_rota_platform/rota/views.py
+++ staff_rota_platform/rota/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login 
+from django.contrib.auth.forms import AuthenticationForm
+from .models import Rota
 
 # Create your views here.
+def index(request):
+    rota = Rota.objects.filter(user=request.user)
+    return render(request, "rota/index.html", {"rota": rota,})
+
+
+@login_required
+def manage_rota(request):
+    if not request.user.is_staff:
+        return redirect("index")
+
+    
+    if request.method == "POST":
+        user_id = request.POST.get("user_id")
+        day = request.POST.get("day")
+        shift_type = request.POST.get("shift_type")
+        start_time = request.POST.get("start_time")
+        end_time = request.POST.get("end_time")
+
+        user = User.objects.get(id=user_id)
+        Rota.object.update_or_create(
+            user=user,
+            day=day,
+            default={"shift_type": shift_type, "start_time": start_time, "end_time": end_time},
+        )
+        return redirect("manage_rota")
+    
+    rota = Rota.objects.all()
+    user = User.objects.all()
+    return render(request, "rota/manage_rota.html", {"roat": rota, "users": users})
+
