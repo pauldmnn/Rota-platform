113ffa0a732d4bf343f3be5e4ff08ef6be8a9b10
Paul-vasile Dominan <pauldmnn@gmail.com>
2024-12-14 15:24:11 +0000

    Registered Rota models to admin panel

--- staff_rota_platform/rota/admin.py
+++ staff_rota_platform/rota/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Rota
 # Register your models here.
+
+@admin.register(Rota)
+class RotaAdmin(admin.ModelAdmin):
+    list_display = ('user', 'day', 'shift_type', 'start_time', 'end_time')
