f96d2ab1d3d570eca2c6a0d724ecc97370e4d716
Paul-vasile Dominan <pauldmnn@gmail.com>
2024-12-19 10:41:56 +0000

    User can change their password

--- rota/urls.py
+++ rota/urls.py
@@ -15,13 +15,13 @@ urlpatterns = [
     path('admin/create_rota/', views.admin_create_rota, name='admin_create_rota'),  # Admin rota creation
     path('admin/update_rota/<int:rota_id>/', views.update_rota, name='update_rota'),  # Update rota
     path('profile/', views.staff_profile, name='staff_profile'),
-]
-
-
-
-urlpatterns += [
+    path('dashboard/', views.staff_dashboard, name='staff_dashboard'),
     path('change_password/', auth_views.PasswordChangeView.as_view(
         template_name='rota/change_password.html',
         success_url='/dashboard/'  # Redirect after successful password change
     ), name='change_password'),
 ]
+
+
+
+
