4c85696735429f439cbcf2063915ab603512508c
Paul-vasile Dominan <pauldmnn@gmail.com>
2024-12-20 13:36:44 +0000

    Add email configuration to password reset

--- staff_rota_platform/settings.py
+++ staff_rota_platform/settings.py
@@ -114,6 +114,14 @@ LOGOUT_REDIRECT_URL = '/login/'
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your-email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your-email-password'
+
+
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-pauldmnn-rotaplatform-bswsshkpux0.ws.codeinstitute-ide.net',
     'https://rota-platform-0319936c5b72.herokuapp.com']
