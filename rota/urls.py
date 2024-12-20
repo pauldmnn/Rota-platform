a4f5d1f7a126b2438ad24abec4118f4ecdcd4511
Paul-vasile Dominan <pauldmnn@gmail.com>
2024-12-20 13:26:06 +0000

    Add Forgoten password to the login page

--- rota/urls.py
+++ rota/urls.py
@@ -24,6 +24,10 @@ urlpatterns = [
         template_name='rota/change_password.html',
         success_url='/dashboard/'  # Redirect after successful password change
     ), name='change_password'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='rota/password_reset.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='rota/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='rota/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset_done/', auth_views.PasswordResetCompleteView.as_view(template_name='rota/password_reset_complete.html'), name='password_reset_complete'),
 ]
 
 
