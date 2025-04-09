570fc14b6e0e7155675dfbeae77556efa3b448ef
pauldmnn <pauldmnn@gmail.com>
2025-04-09 12:27:03 +0100

    Added load static to all html files

--- staff_rota_platform/settings.py
+++ staff_rota_platform/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-pauldmnn-rotaplatform-bswsshkpux0.ws.codeinstitute-ide.net',
+ALLOWED_HOSTS = ['127.0.0.1',
                 'https://rota-platform-0319936c5b72.herokuapp.com/',
                 '.herokuapp.com']
 
